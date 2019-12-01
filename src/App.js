a18a681aa8dd80293998e9f65bd76d7732ef8b0b
lohyinghao <lohyinghao91@gmail.com>
2019-12-01 22:12:05 +0800

    Refactor to use class and use contextApi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import DataLoader from './dataLoader/DataLoader';
 
-function App() {
-  return (
-    <div className='App'>
-      <DataLoader />
-    </div>
-  );
+export const MyContext = React.createContext();
+
+class JsonProvider extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      json:
+        '{"zdtiic":["JsZbxHqM",-1933451531,true,[false,false,-400544804.8848603,"bTJ9sonw","c"]],"ujnjsnjpwra":[[329398297.34234834]],"eslygpyawkgs":351427425.0266722,"puyqhywhfh":false,"wltdqoduhjex":1175683688.9913287}'
+    };
+
+    this.handleChange = e => {
+      this.setState({
+        json: e.target.value
+      });
+    };
+
+    this.formatJson = () => {
+      //e.preventDefault();
+      this.setState({
+        json: JSON.stringify(JSON.parse(this.state.json), null, 2)
+      });
+    };
+  }
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          handleChange: this.handleChange,
+          formatJson: this.formatJson
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <JsonProvider>
+        <div className='App'>
+          <DataLoader />
+        </div>
+      </JsonProvider>
+    );
+  }
 }
 
 export default App;
--- src/dataLoader/DataLoader.js
+++ src/dataLoader/DataLoader.js
@@ -1,37 +1,26 @@
-import React, { Fragment, useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment, Component } from 'react';
 import './DataLoader.css';
+import { MyContext } from './../App';
 
-const DataLoader = props => {
-  const [formData, setFormData] = useState({
-    value:
-      '{"zdtiic":["JsZbxHqM",-1933451531,true,[false,false,-400544804.8848603,"bTJ9sonw","c"]],"ujnjsnjpwra":[[329398297.34234834]],"eslygpyawkgs":351427425.0266722,"puyqhywhfh":false,"wltdqoduhjex":1175683688.9913287}'
-  });
-
-  const handleChange = e => {
-    setFormData({ value: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('handleSubmit');
-    setFormData({ value: e.target.value });
-    console.log(JSON.stringify(JSON.parse(formData.value), null, 2));
-    setFormData({ value: JSON.stringify(JSON.parse(formData.value), null, 2) });
-  };
-
-  return (
-    <Fragment>
-      {' '}
-      <a>Input Json</a>
-      <form onSubmit={e => handleSubmit(e)}>
-        <textarea value={formData.value} onChange={e => handleChange(e)} />
-        <input type='submit' value='Submit' />
-      </form>
-    </Fragment>
-  );
-};
-
-DataLoader.propTypes = {};
+class DataLoader extends Component {
+  render() {
+    return (
+      <MyContext.Consumer>
+        {context => (
+          <Fragment>
+            <a>Input Json</a>
+            <form>
+              <textarea
+                value={context.state.json}
+                onChange={e => context.handleChange(e)}
+              />
+            </form>
+            <button onClick={context.formatJson}>Format</button>
+          </Fragment>
+        )}
+      </MyContext.Consumer>
+    );
+  }
+}
 
 export default DataLoader;
