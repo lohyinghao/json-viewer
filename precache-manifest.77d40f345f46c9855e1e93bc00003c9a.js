9784093e0e121914197fcc27c483d92990458ac7
lohyinghao <lohyinghao91@gmail.com>
2019-12-08 18:17:15 +0800

    Updates

---
+++ precache-manifest.77d40f345f46c9855e1e93bc00003c9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cafe7e3b75b2bfcb4e696d79096369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7019d51776efaf31fc5b",
+    "url": "/static/css/main.5f0ef462.chunk.css"
+  },
+  {
+    "revision": "18cde2cd1e770c459358",
+    "url": "/static/js/2.e4ede52e.chunk.js"
+  },
+  {
+    "revision": "7019d51776efaf31fc5b",
+    "url": "/static/js/main.bbb5fa59.chunk.js"
+  },
+  {
+    "revision": "bae5bd87a99a1b3b0ed4",
+    "url": "/static/js/runtime-main.94d7c657.js"
+  }
+]);
